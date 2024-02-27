f0dbba366812fe30d0adc36d673c245d9ae9c756
Qtty <lyes.mouloudi.007@gmail.com>
2024-02-27 22:07:14 +0100

    Added a conception MD and first prototype of the backend

---
+++ contracts/PasswordManager.sol
@@ -0,0 +1,233 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+pragma solidity ^0.8.20;
+
+import "fhevm/abstracts/Reencrypt.sol";
+import "fhevm/lib/TFHE.sol";
+
+contract PasswordManager is Reencrypt {
+    address public admin;
+    uint256 public constant PASSWORD_LENGTH = 4; // Example length, adjust as needed
+    uint256 public constant TITLE_LENGTH_LIMIT = 15;
+    uint256 public constant DESCRIPTION_LENGTH_LIMIT = 25;
+
+    struct Item {
+        uint256 id;
+        string title;
+        string description;
+        uint256 vault;
+        // Assuming 'password' is a simplified representation
+        uint32[] password;
+    }
+
+    // List of vault IDs
+    uint256[] private vaultIds;
+
+    // Mapping from vault ID to vault name
+    mapping(uint256 => string) private vaultNames;
+
+    // Mapping from vault ID to list of Item IDs
+    mapping(uint256 => uint256[]) private vaults;
+
+    // Mapping from item ID to Item
+    mapping(uint256 => Item) private items;
+
+    // Unique identifier for items
+    uint256 private itemIdCounter = 0;
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Caller is not the admin");
+        _;
+    }
+
+    modifier itemExists(uint256 _itemId) {
+        require(items[_itemId].id != 0, "Item does not exist");
+        _;
+    }
+
+    constructor(address _admin) {
+        admin = _admin;
+        createVault("All");
+    }
+
+    function createVault(string memory _name) public onlyAdmin returns (uint256) {
+        require(bytes(_name).length > 0, "Vault name cannot be empty");
+        // Assuming vaultIdCounter is a contract-wide unique identifier for vaults
+        uint256 newVaultId = vaultIds.length;
+        vaultNames[newVaultId] = _name;
+        vaultIds.push(newVaultId);
+        return newVaultId;
+    }
+
+    function createItem(
+        string memory _title,
+        string memory _description,
+        uint256 _vaultId,
+        uint32[] memory _password
+    ) public onlyAdmin returns (uint256) {
+        require(bytes(_title).length > 0, "Title cannot be empty");
+        require(bytes(_title).length <= TITLE_LENGTH_LIMIT, "Title cannot exceed 15 characters");
+        if (bytes(_description).length > 0) {
+            require(bytes(_description).length <= DESCRIPTION_LENGTH_LIMIT, "Description cannot exceed 25 characters");
+        }
+        if (_vaultId != 0) {
+            // Assuming 0 is not a valid vault ID and is used to indicate no vault
+            require(vaultExists(_vaultId), "Vault ID is not valid");
+        }
+        if (_password.length == 0) {
+            _password = generateNewPassword();
+        }
+
+        // Create the item
+        Item memory newItem = Item({
+            id: itemIdCounter,
+            title: _title,
+            description: _description,
+            vault: _vaultId, // Ensure your Item struct has a vault_id attribute
+            password: _password
+        });
+
+        items[itemIdCounter] = newItem;
+        if (_vaultId != 0) {
+            vaults[_vaultId].push(itemIdCounter);
+        }
+        itemIdCounter++;
+        return itemIdCounter - 1;
+    }
+
+    // Utility function to check if a vault exists
+    function vaultExists(uint256 _vaultId) private view returns (bool) {
+        for (uint256 i = 0; i < vaultIds.length; i++) {
+            if (vaultIds[i] == _vaultId) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function getVaults() public view onlyAdmin returns (uint256[] memory, string[] memory) {
+        string[] memory names = new string[](vaultIds.length);
+        for (uint i = 0; i < vaultIds.length; i++) {
+            names[i] = vaultNames[vaultIds[i]];
+        }
+        return (vaultIds, names);
+    }
+
+    function getItemsOfVault(uint256 _vaultId) public view onlyAdmin returns (uint256[] memory, string[] memory) {
+        if (_vaultId == 0) {
+            // Handle the default vault case
+            uint256 itemCount = itemIdCounter - 1; // Assuming itemIdCounter is incremented for each new item
+            uint256[] memory ids = new uint256[](itemCount);
+            string[] memory titles = new string[](itemCount);
+            for (uint256 i = 1; i <= itemCount; i++) {
+                if (items[i].id != 0) {
+                    // Check if item exists
+                    ids[i - 1] = items[i].id;
+                    titles[i - 1] = items[i].title;
+                }
+            }
+            return (ids, titles);
+        } else {
+            // Original logic for specific vaults
+            uint256[] memory ids = vaults[_vaultId];
+            string[] memory titles = new string[](ids.length);
+            for (uint i = 0; i < ids.length; i++) {
+                titles[i] = items[ids[i]].title;
+            }
+            return (ids, titles);
+        }
+    }
+
+    function getItem(uint256 _itemId) public view onlyAdmin itemExists(_itemId) returns (Item memory) {
+        return items[_itemId];
+    }
+
+    // Simplified version without encryption details
+    function getPlainPassword(
+        uint256 _itemId,
+        bytes32 publicKey,
+        bytes calldata signature
+    ) public view onlyAdmin itemExists(_itemId) onlySignedPublicKey(publicKey, signature) returns (bytes memory) {
+        // TODO: return a re-encryption of each value in the password array
+        return "test"; //TFHE.reencrypt(items[_itemId].password[0], publicKey);
+    }
+
+    function deleteItem(uint256 _itemId) public onlyAdmin itemExists(_itemId) {
+        // Retrieve the vault ID associated with this item
+        uint256 vaultId = items[_itemId].vault;
+
+        // Remove the item from its vault
+        // Assuming vaults mapping is from vault ID to an array of item IDs
+        uint256[] storage itemIds = vaults[vaultId];
+        for (uint256 i = 0; i < itemIds.length; i++) {
+            if (itemIds[i] == _itemId) {
+                // Remove the item ID from the array by swapping it with the last element and then popping the array
+                itemIds[i] = itemIds[itemIds.length - 1];
+                itemIds.pop();
+                break;
+            }
+        }
+
+        // Remove the item from the items mapping
+        delete items[_itemId];
+    }
+
+    function setItem(
+        uint256 _itemId,
+        string memory _title,
+        string memory _description,
+        uint256 _vaultId,
+        uint32[] memory _newPassword
+    ) public onlyAdmin itemExists(_itemId) {
+        Item storage item = items[_itemId];
+
+        // Check and update the title if not empty and within length limit
+        if (bytes(_title).length > 0 && bytes(_title).length <= TITLE_LENGTH_LIMIT) {
+            item.title = _title;
+        }
+
+        // Check and update the description if not empty and within length limit
+        if (bytes(_description).length > 0 && bytes(_description).length <= DESCRIPTION_LENGTH_LIMIT) {
+            item.description = _description;
+        }
+
+        // Check and update the password if not empty
+        if (_newPassword.length > 0) {
+            item.password = _newPassword;
+        }
+
+        // Check and update the vault, ensuring to remove the item from the old vault if necessary
+        if (_vaultId != 0 && vaultExists(_vaultId)) {
+            // Remove item from the old vault if it's different
+            if (item.vault != _vaultId) {
+                removeItemFromVault(_itemId, item.vault);
+                item.vault = _vaultId;
+                vaults[_vaultId].push(_itemId);
+            }
+        }
+    }
+
+    // Utility function to remove an item from a specific vault
+    function removeItemFromVault(uint256 _itemId, uint256 _vaultId) private {
+        uint256[] storage itemsInVault = vaults[_vaultId];
+        for (uint256 i = 0; i < itemsInVault.length; i++) {
+            if (itemsInVault[i] == _itemId) {
+                itemsInVault[i] = itemsInVault[itemsInVault.length - 1];
+                itemsInVault.pop();
+                break;
+            }
+        }
+    }
+
+    function generateNewPassword() public view onlyAdmin returns (uint32[] memory) {
+        uint32[] memory newPassword = new uint32[](PASSWORD_LENGTH);
+        for (uint i = 0; i < PASSWORD_LENGTH; i++) {
+            newPassword[i] = uint32(uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, i))) % 4294967295);
+        }
+        return newPassword;
+    }
+
+    function destroy() public onlyAdmin {
+        selfdestruct(payable(admin));
+    }
+}
---
+++ task.md
@@ -0,0 +1,145 @@
+# FhVault
+
+## Goals
+
+### Main Goals
+
+Each contract will be exclusively to owned by one user, and after finishing these main goals, we'll create a proxy
+contract that acts as a gateway for users to create and get the contract addresses of their password manager.
+
+1. User authenticates with his wallet -> constructor()
+
+2. When authenticated, the user will be presented with: the list of his vaults, a middle screen and buttons on top to
+   create items and vaults -> getVaults()
+
+3. when a vault is selected, the items are enumerated in the middle screen -> getItemsOfVault()
+
+4. when an item is selected:
+
+   1. it's info are presented -> getItem()
+   2. a button to reveal the password -> getPlainPassword()
+   3. a button to delete the item -> deleteItem()
+   4. a button to edit the informations -> setItem()
+   5. a button to generate a new random password -> generateNewPassword() + setItem()
+
+5. there'll be a back button to get back to the enumerated
+
+6. When creating a new item: -> createItem()
+
+   1. add a title (mandatory)
+   2. add a description (optional)
+   3. add a vault from the list of existing vaults(optional)
+   4. add a password, or generate a random one (mandatory) -> generateNewPassword()
+
+7. When creating a new vault: -> createVault()
+
+   1. add a vault name (mandatory)
+
+8. delete the password manager. -> selfdestruct()
+
+### Future Goals
+
+1. Each time a user connects for the first time, a new contract is created only for him
+
+## Technical Structure
+
+- constructor(address \_admin):
+
+  1. set an admin which is the user address
+  2. this admin will be used in onlyAdmin modifier to maintain the exclusivity of the contract to the user
+  3. all the functions will use the modifier onlyAdmin
+
+- getVaults():
+
+  1. returns the list of vaults
+
+- getItemsOfVault(uint \_vault_id):
+
+  1. grabs the list of item IDs that are present in the desired vault
+  2. returns a list of item IDs and item titles by getting the title of the item based on it's item ID
+
+- getItem(uint \_item_id):
+
+  1. gets the Item using it's item_id and returns it to the user
+
+- getPlainPassword(uint \_item_id, bytes32 publicKey, bytes calldata signature):
+
+  1. uses onlySignedPublicKey(publicKey, signature) to verify that the user is really him
+  2. reencrypts the password with the user public key and returns it
+
+- deleteItem(uint \_item_id, uint \_vault_id):
+
+  1. removes the item from the it's vault
+  2. removes the item from the Items
+
+- setItem(uint \_item_id, string \_title, string \_description, string \_vault, List[euint32] \_new_password):
+
+  1. checks which attributes to be set, the others will be null (one or many of them)
+  2. makes the appropriate checks for the attribute to be set: length of the title or description, and the existance of
+     the vault, correctness of the euint32s.
+  3. stores the changes in the item using it's item ID
+
+- modifier \_item_exists(uint \_item_id):
+
+  1. checks whether an item really exists, to be used in the functions that manipulate existing items
+
+- generateNewPassword():
+
+  1. uses TFHE.randomEuint32() to generate PASSWORD_LENGTH random values that will be used as the new password.
+  2. PASSWORD_LENGTH will be a static attribute in the contract.
+  3. returns List[euint32].
+
+- createItem(string \_title, string \_description, uint \_vault_id, List[euint32] \_password):
+
+  1. checks whether title is of the right length(a limit will be set in the contract), the title is mandatory.
+  2. checks, if not empty, that the description is of the right length(a different limit will be set in the contract),
+     the description is optional.
+  3. checks, if not empty, that the vault ID truly exists in the vault list.
+  4. if password is null, calls generateNewPassword() to generate a random new password.
+  5. creates an Item object with these info and attribute an item ID to it.
+  6. adds the item ID to the vault if a vault was given.
+  7. returns the item ID
+
+- createVault(string \_name):
+
+  1. checks the length if the name.
+  2. creates a new ID for the vault.
+  3. maps the id to the name in the vault mapping
+  4. returns the ID
+
+- selfdestruct():
+
+  1. destroys the contract.
+  2. in the future, calls the proxy contract to delete the address of this contract.
+
+## Notes
+
+Item { title<string> description<string> (optional) vault<string> (optional) value<List[euint32]> }
+
+- Check the case where an attacker grabs an encrypted value from the public storage, puts in his own smart contract and
+  then decrypts it. OK
+
+- Instead of having the vaults inside the contract as a struct, we'll have them be in the item as a string and the
+  frontend will do the filtering using the string. This way we'll avoid having more gas poured into transactions.
+
+- Login will be made with the user' wallet, he'll agree to use his wallet and then he'll be presented the dashboard,
+  it'll contain:
+
+  1. a topbar, where there'll be a create a new item button
+  2. a sidebar where there' gonna be the vaults and the default vault where all the items are
+  3. the main section in the middle where he'll be shown the items inside the vault 4. when he clicks on an item, he'll
+     be presented with the info of that item and a button to get the value 5. That button will trigger a reencryption in
+     the smart contract, and the decryption will happen on the client side 6. there' also an edit button to change the
+     value of the password, or any other value about the item(title, description, vault)
+  4. Password generator?
+  5. Browser extension with auto-fill
+
+- They're basically protecting using the ZK thing, so an attacker has to go through asEuintX to use a ciphertext that he
+  got from the storage, but this ciphertext won't pass the ZK test to prove that the user knows the plaintext but what
+  if we grab the ciphertext from the storage(it's a uint256) and inject it in the storage of our controlled smart
+  contract as a EuintX, so we'll already have a euint8 in the memory and we'll inject our value there thus bypassing the
+  ZK verification of asEuintX. OK
+
+- The previous suggestion won't work due to FhEVM structure, the euints are only handlers, hashes to the actual
+  ciphertexts, and these hashes are used to map the handles to the actual ciphertext, and the ciphertext are stored in a
+  priviliged storage that the contract cannot access, only the EVM does.
